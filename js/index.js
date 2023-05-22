84f5bc2ad67d8be225bf683a5ef54f2e7ba1400c
Luxur90 <prueba@correo.com>
2023-05-22 14:40:15 -0500

    Feat: ocultar menu con CSS y JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-
+function mostrar(){
+    document.getElementById('barranav').style.display = 'block';
+    document.getElementById('btmostrar').style.display = 'none';
+    document.getElementById('btocultar').style.display = 'block';
+}
+function ocultar(){
+    document.getElementById('barranav').style.display = 'none';
+    document.getElementById('btmostrar').style.display = 'block';
+    document.getElementById('btocultar').style.display = 'none';
+    
+}
 
 
