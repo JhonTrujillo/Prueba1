1192190761751fb0d36b7f70859c9af125783d37
JhonHTrujillo <jhonhtg90@gmail.com>
2023-06-10 15:53:12 -0500

    Feat: se crean hiperviculos en las card y pagina html a cada card

--- js/card.js
+++ js/card.js
@@ -4,45 +4,44 @@ const programas = [
         titulo:'Primaria',
         imagen:'images/primaria.jpg',
         descripcion:'Niveles comprendidos de jardin a quinto',
-        enlace:'',
-
+        enlace:'primaria.html',
     },
     {
         titulo:'Secundaria',
         imagen:'images/secundaria.avif',
         descripcion:'Niveles comprendidos de sexto a once',
-        Enlace:'',
+        enlace:'secundaria.html',
     },
     {
-        titulo:'Cursos vacacionales',
+        titulo:'Cursos',
         imagen:'images/clasesvacacionales.jpg',
         descripcion:'Cursos de refuerzo',
-        Enlace:'',
+        enlace:'cursos.html',
     }
-    
 ];
 
 let card = document.getElementById("contcard")
 
 for(let i = 0; i < programas.length; i++){
     let creatarjeta = document.createElement("div");
+    creatarjeta.className = "creartarjeta";
         let titulo = document.createElement("h3");
         let imagen = document.createElement("img");
         let descripcion = document.createElement("p");
-        // let enlace = document.createElement("a");  
+        let enlace = document.createElement("a");
         
         titulo.textContent = programas[i].titulo;
         imagen.setAttribute('src', programas[i].imagen);
         descripcion.textContent = programas[i].descripcion; 
-        // enlace.setAttribute("href=matriculate.html", programas[i].enlace);
-
+        enlace.setAttribute('href', programas[i].enlace);
+        enlace.textContent = ("Click Aqui");
+                     
         creatarjeta.appendChild(titulo);
         creatarjeta.appendChild(imagen);
         creatarjeta.appendChild(descripcion);
-        // creatarjeta.appendChild(enlace);
+        creatarjeta.appendChild(enlace);
         card.appendChild(creatarjeta);
         
-    
-    
-
 }
+
+ 
